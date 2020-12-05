9cf5970826653b0914f780a9d1426625a8d2ee54
Matt Radtke <more>
2020-12-04 20:01:49 -0600

    Code

--- TravelingSalesman/TravelingSalesman.py
+++ TravelingSalesman/TravelingSalesman.py
@@ -1,9 +1,28 @@
 import plotly.graph_objects as go
 import numpy as np
+import json, urllib
+from urllib.parse import urlencode
+import urllib.request
+import googlemaps
+
+# need to calculate shortest path from A to B
+# Store route and cost of this path
 
 def code():
-    fig = go.Figure(data=go.Bar(y=[2, 3, 1]))
-    fig.write_html('first_figure.html', auto_open=True)
+
+    start = "Bridgewater, Sa, Australia"
+    finish = "Stirling, SA, Australia"
+
+    url = 'http://maps.googleapis.com/maps/api/directions/json?%s' % urlencode((
+                ('origin', start),
+                ('destination', finish)
+     ))
+    with urllib.request.urlopen(url) as ur:
+        result = json.load(ur)
+
+    for i in range (0, len (result['routes'][0]['legs'][0]['steps'])):
+        j = result['routes'][0]['legs'][0]['steps'][i]['html_instructions'] 
+        print(j)
 
 def main():
     code()
